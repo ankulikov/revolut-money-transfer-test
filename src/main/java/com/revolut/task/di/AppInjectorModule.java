d4409f13d7340d1eb5c96f065aeae8d403d55a4f
Andrey Kulikov <dev@ankulikov.me>
2018-09-26 22:19:52 +0300

    Configure DI for all services

--- src/main/java/com/revolut/task/di/AppInjectorModule.java
+++ src/main/java/com/revolut/task/di/AppInjectorModule.java
@@ -1,15 +1,17 @@
 package com.revolut.task.di;
 
 import com.google.inject.AbstractModule;
-import com.revolut.task.service.api.AccountService;
-import com.revolut.task.service.api.DatabaseManager;
-import com.revolut.task.service.impl.AccountServiceImpl;
-import com.revolut.task.service.impl.DatabaseManagerImpl;
+import com.revolut.task.service.api.*;
+import com.revolut.task.service.impl.*;
 
 public class AppInjectorModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(DatabaseManager.class).to(DatabaseManagerImpl.class);
+        bind(AccountLockManager.class).to(AccountLockManagerImpl.class);
+        bind(ExchangeService.class).to(ExchangeServiceImpl.class);
         bind(AccountService.class).to(AccountServiceImpl.class);
+        bind(MoneyService.class).to(MoneyServiceImpl.class);
+
     }
 }
