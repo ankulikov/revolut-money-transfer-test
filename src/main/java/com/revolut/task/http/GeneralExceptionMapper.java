697060ca04b73688f34000f51df17e390d86fd98
Andrey Kulikov <dev@ankulikov.me>
2018-09-27 23:05:53 +0300

    Set response status code depending on Exception class

--- src/main/java/com/revolut/task/http/GeneralExceptionMapper.java
+++ src/main/java/com/revolut/task/http/GeneralExceptionMapper.java
@@ -1,17 +1,31 @@
 package com.revolut.task.http;
 
+import com.revolut.task.model.Error;
+import com.revolut.task.model.exceptions.AccountNotFoundException;
+import com.revolut.task.model.exceptions.MoneyNegativeAmountException;
+
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import java.util.HashMap;
+
+import static javax.ws.rs.core.Response.Status;
+import static javax.ws.rs.core.Response.status;
 
 @Provider
 public class GeneralExceptionMapper implements ExceptionMapper<Exception> {
 
     @Override
     public Response toResponse(Exception e) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("error", e.getMessage());
-        return Response.serverError().entity(map).build();
+        Status status = Status.INTERNAL_SERVER_ERROR;
+        if (e instanceof AccountNotFoundException) {
+            status = Status.NOT_FOUND;
+        } else if (e instanceof MoneyNegativeAmountException) {
+            status = Status.BAD_REQUEST;
+        }
+        return status(status)
+                .entity(new Error(e.getMessage()))
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .build();
     }
 }
---
+++ src/main/java/com/revolut/task/model/Error.java
@@ -0,0 +1,16 @@
+package com.revolut.task.model;
+
+public class Error {
+    private String error;
+
+    public Error() {
+    }
+
+    public Error(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
